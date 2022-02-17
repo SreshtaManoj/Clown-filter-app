3d9eed38cf6b7916ea6a3c71a34c40971344fcb7
SreshtaManoj <96248779+SreshtaManoj@users.noreply.github.com>
2022-02-17 21:19:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+noseX=0;
+noseY=0
+function preload(){
+    clown_nose = loadImage('https://i.postimg.cc/FRRLf2DH/clown-nose.jpg')
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function gotPoses(results)
+{
+    console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("nose x = " + noseX);
+    console.log("nose y = " + noseY);
+}
+function modelLoaded(){
+    console.log('PoseNet is intialized');
+}
+function draw(){
+    image(video, 0, 0, 300, 300,);
+fill("#66e617");
+stroke("#66e617");
+image(clown_nose, noseX, noseY, 30, 30);
+}
+function take_snapshot(){
+    save('myFilterImage.png');
+}
